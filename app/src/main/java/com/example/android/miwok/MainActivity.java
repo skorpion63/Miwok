0d919fc8422ace8c2020c9dc4589e35422654535
jangjeet chauhan <skorpion63@gmail.com>
2016-08-20 15:00:08 +0530

    Implemented Onclick listners for all relevent text views.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,12 +29,62 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        //Find the view that shows numbers category.
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+
+        //Find the view that shows Family members.
+        TextView family = (TextView) findViewById(R.id.family);
+
+        //Find the view that shows colors.
+        TextView colors = (TextView) findViewById(R.id.colors);
+
+        //Find the view that shows phrases.
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+
+        //Create a onclicklistner to handle the numbers click and open numbersActivity.
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent numbers = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbers);
+
+            }
+        });
+
+        //Create a onclicklistner to handle the Family view click and open familyActivity.
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent family = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(family);
+
+            }
+        });
+
+        //Create a onclicklistner to handle the colors click and open colorActivity.
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent colors = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(colors);
+
+            }
+        });
+
+        //Create a onclicklistner to handle the Phrases click and open phrasesActivity.
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent phrases = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(phrases);
+
+
+            }
+        });
+
+
     }
 
-    //Opens numbersList when user clicks Numbers.
-    public void openNumbersList(View view){
-        Intent numbers = new Intent(this, NumbersActivity.class);
-        startActivity(numbers);
 
-    }
 }
