d41609e150c053a9d5bdcb448ef17479865ac6f5
jangjeet chauhan <skorpion63@gmail.com>
2016-08-20 21:23:44 +0530

    Added numbers arraylist and created dynamic textViews to display the numbers.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,48 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Add a string arrayList to store the english translation of numbers.
+        ArrayList<String> words = new ArrayList<String>();
+
+        //Initialize the arrayList with numbers.
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
+        //Find the nuberActivity layout by id.
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        //create a variable to keep track of indices.
+        int i = 0;
+
+        //Add child views to LinearLayout till the arraylist is over.
+        while (i < words.size()){
+
+            //Create a new TextView.
+            TextView wordView = new TextView(this);
+
+            //Set the textView to the current word in the arrylist.
+            wordView.setText(words.get(i));
+
+            //add the textview as a child to the rootview.
+            rootView.addView(wordView);
+
+            //increase the variable by 1.
+            i++;
+
+
+        }
+
+
+
+
     }
 }
