54048be5d73fd21f4647dbcdba0ea57b8b2ebe42
jangjeet chauhan <skorpion63@gmail.com>
2016-08-24 19:27:21 +0530

    added images to all the activities.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -7,10 +7,18 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     public Word(String defaultTranslation, String miwokTranslation){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
     }
 
     public String getmDefaultTranslation(){
@@ -19,6 +27,12 @@ public class Word {
 
     public  String getmMiwokTranslation(){
         return mMiwokTranslation;
+    }
+
+    public int getmImageResourceId(){return mImageResourceId;}
+
+    public boolean hasImage(){
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.app.Activity;
 import android.provider.UserDictionary;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +17,15 @@ import java.util.ArrayList;
  */
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    private int mBackColor;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int color) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, words);
+        mBackColor = color;
     }
 
     /**
@@ -40,6 +44,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
         if(listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
+
         }
 
         // Get the {@link AndroidFlavor} object located at this position in the list
@@ -56,6 +61,19 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // Get the version number from the current AndroidFlavor object and
         // set this text on the number TextView
         defaultTextView.setText(currentWord.getmDefaultTranslation());
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image_view);
+
+        if (currentWord.hasImage()) {
+            imageView.setImageResource(currentWord.getmImageResourceId());
+        }
+        else{
+            imageView.setVisibility(View.GONE);
+        }
+
+        View textContainer = listItemView.findViewById(R.id.textContainer);
+        int colo = ContextCompat.getColor(getContext(),mBackColor);
+        textContainer.setBackgroundColor(colo);
 
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
